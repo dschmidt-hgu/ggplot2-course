2477ee2bc7d56cd5929994143cb6efdb0880f3e0
dschmidt-hgu <dominik.schmidt@hs-gm.de>
2020-12-01 14:38:57 +0100

    course material added

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ggplot2-course
 Graduate School Course Material
+
+https://mybinder.org/v2/gh/dschmidt-hgu/ggplot2-course/main
+
+https://mybinder.org/v2/gh/dschmidt-hgu/ggplot2-course/main?urlpath=rstudio
--- install.R
+++ install.R
@@ -1,3 +1,15 @@
-install.packages("tidyverse")
-install.packages("rmarkdown")
-install.packages("data.table")
+install.packages("rmarkdown") 	# to compile RMD files
+install.packages("ggplot2")		# plotting
+install.packages("data.table")	# data management
+#install.packages("rdwd")		# get DWD climate data (optional)
+remotes::install_github("brry/rdwd", build_vignettes=TRUE) # for latest version
+install.packages("patchwork")	# align multiple plots
+install.packages("scales")      # pretty scales
+install.packages("ggforce")     # zoom facet, annotation
+install.packages("transformr")  # req. for gganimate example
+install.packages("sf")          # req. for gganmiate example
+install.packages("gganimate")   # animate ggplot2s
+install.packages("ggthemes")    # themes for ggplot2
+install.packages("rgl")         # 3D plotting
+install.packages("rglwidget")   # 3D plotting in HTML
+install.packages("plotly")      # Interactive graphs
