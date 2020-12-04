78368bc98d2dc4c011f5ff1a7bc730daa6dccfb5
dschmidt-hgu <dominik.schmidt@hs-gm.de>
2020-12-04 13:02:11 +0100

    apt package mod

--- install.R
+++ install.R
@@ -1,5 +1,5 @@
 #install.packages("devtools") 	# 
-#install.packages("rmarkdown") 	# to compile RMD files
+install.packages("rmarkdown") 	# to compile RMD files
 install.packages("rmdformats") 	# to compile RMD files
 #install.packages("ggplot2")		# plotting apt install
 #install.packages("data.table")	# data management
@@ -8,9 +8,9 @@ install.packages("patchwork")	# align multiple plots
 #install.packages("scales")      # pretty scales
 install.packages("transformr")  # req. for gganimate example
 #install.packages("sf")          # req. for gganmiate example
-#install.packages("ggforce")     # zoom facet, annotation
+install.packages("ggforce")     # zoom facet, annotation
 install.packages("gganimate")   # animate ggplot2s
-#install.packages("ggthemes")    # themes for ggplot2
+install.packages("ggthemes")    # themes for ggplot2
 #install.packages("rgl")         # 3D plotting
 #install.packages("plotly")      # Interactive graphs
 
