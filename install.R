032033afd57b040628de41b029bf22a9700186da
dschmidt-hgu <dominik.schmidt@hs-gm.de>
2020-12-04 11:55:48 +0100

    test for faster installation script/apt packages

--- install.R
+++ install.R
@@ -1,16 +1,17 @@
-install.packages("rmarkdown") 	# to compile RMD files
-install.packages("ggplot2")		# plotting
-install.packages("data.table")	# data management
-#install.packages("remotes")		# get DWD climate data (optional)
-#remotes::install_github("brry/rdwd", build_vignettes=FALSE,build=FALSE) # for latest version
+#install.packages("devtools") 	# 
+#install.packages("rmarkdown") 	# to compile RMD files
+install.packages("rmdformats") 	# to compile RMD files
+#install.packages("ggplot2")		# plotting apt install
+#install.packages("data.table")	# data management
 install.packages("rdwd")		# get DWD climate data (optional)
 install.packages("patchwork")	# align multiple plots
-install.packages("scales")      # pretty scales
-install.packages("ggforce")     # zoom facet, annotation
+#install.packages("scales")      # pretty scales
 install.packages("transformr")  # req. for gganimate example
-install.packages("sf")          # req. for gganmiate example
+#install.packages("sf")          # req. for gganmiate example
+#install.packages("ggforce")     # zoom facet, annotation
 install.packages("gganimate")   # animate ggplot2s
-install.packages("ggthemes")    # themes for ggplot2
-install.packages("rgl")         # 3D plotting
-install.packages("rglwidget")   # 3D plotting in HTML
-install.packages("plotly")      # Interactive graphs
+#install.packages("ggthemes")    # themes for ggplot2
+#install.packages("rgl")         # 3D plotting
+#install.packages("plotly")      # Interactive graphs
+
+
