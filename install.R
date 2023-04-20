ac7ef475c86817056d5fb595ee2dd4774cfa7cfd
dschmidt-hgu <dominik.schmidt@hs-gm.de>
2023-04-20 14:57:34 +0200

    update for 2023

--- install.R
+++ install.R
@@ -1,28 +1,22 @@
-usePackage <- function(p)   
-{ 
-  if (!is.element(p, installed.packages()[,1])) 
-    install.packages(p, dep = TRUE, repos="https://cloud.r-project.org/", lib=.libPaths()[1]) 
-  require(p, character.only = TRUE) 
-} 
-usePackage("devtools") 	# 
-usePackage("rmarkdown")     # to compile RMD files
-usePackage("rmdformats") 	# to compile RMD files
-usePackage("ggplot2")  		# plotting
-usePackage("data.table")    # data management
-usePackage("rdwd")         # get DWD climate data (optional)
-#usePackage("remotes")         # for latest version of rdwd
-#remotes::install_github("brry/rdwd", build_vignettes=TRUE) # for latest version  of rdwd
-usePackage("patchwork")     # align multiple plots
-usePackage("scales")        # pretty scales
-usePackage("ggforce")       # zoom facet, annotation
-usePackage("sf")            # req. for gganmiate example
-usePackage("transformr")    # req. for gganimate example
-usePackage("gganimate")     # animate ggplot2s
-usePackage("ggthemes")      # themes for ggplot2
-usePackage("rgl")           # 3D plotting
-usePackage("plotly")        # Interactive graphs
-usePackage("gghalves")      # half-box-plot etc.
-usePackage("lvplot")        # boxenplot/letter value plot
-usePackage("ggridges")      # ridgeline plots
-usePackage("ggpubr")        # helper functions
-usePackage("ggdist")        # distribution plots
+if (!require("pacman")) install.packages("pacman")
+
+# install/load packages using pacman package
+pacman::p_load("rmarkdown")     # to compile RMD files
+pacman::p_load("rmdformats") 	  # to compile RMD files
+pacman::p_load("ggplot2")  		  # plotting
+pacman::p_load("data.table")    # data management
+pacman::p_load("rdwd")          # get DWD climate data (optional)
+pacman::p_load("patchwork")     # align multiple plots
+pacman::p_load("scales")        # pretty scales
+pacman::p_load("ggforce")       # zoom facet, annotation
+pacman::p_load("sf")            # req. for gganmiate example
+pacman::p_load("transformr")    # req. for gganimate example
+pacman::p_load("gganimate")     # animate ggplot2s
+pacman::p_load("ggthemes")      # themes for ggplot2
+pacman::p_load("rgl")           # 3D plotting
+pacman::p_load("plotly")        # Interactive graphs
+pacman::p_load("gghalves")      # half-box-plot etc.
+pacman::p_load("lvplot")        # boxenplot/letter value plot
+pacman::p_load("ggridges")      # ridgeline plots
+pacman::p_load("ggpubr")        # helper functions
+pacman::p_load("ggdist")        # distribution plots
