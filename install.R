1123a50a124d95106f998cfcb7bcfebb71fa89cd
dschmidt-hgu <dominik.schmidt@hs-gm.de>
2024-04-19 14:53:40 +0200

    update 2024

--- install.R
+++ install.R
@@ -1,15 +1,12 @@
 if (!require("pacman")) install.packages("pacman")
 
 # install/load packages using pacman package
-pacman::p_load("rmarkdown")     # to compile RMD files
-pacman::p_load("rmdformats") 	  # to compile RMD files
 pacman::p_load("ggplot2")  		  # plotting
 pacman::p_load("data.table")    # data management
 pacman::p_load("rdwd")          # get DWD climate data (optional)
 pacman::p_load("patchwork")     # align multiple plots
-pacman::p_load("scales")        # pretty scales
+pacman::p_load("scales")        # (pretty) scales + helpers
 pacman::p_load("ggforce")       # zoom facet, annotation
-pacman::p_load("sf")            # req. for gganmiate example
 pacman::p_load("transformr")    # req. for gganimate example
 pacman::p_load("gganimate")     # animate ggplot2s
 pacman::p_load("ggthemes")      # themes for ggplot2
@@ -20,3 +17,10 @@ pacman::p_load("lvplot")        # boxenplot/letter value plot
 pacman::p_load("ggridges")      # ridgeline plots
 pacman::p_load("ggpubr")        # helper functions
 pacman::p_load("ggdist")        # distribution plots
+pacman::p_load("skimr")         # complex data summary (optional)
+pacman::p_load("hexbin")        # for geom_hex
+pacman::p_load("scico")         # scientific color maps
+pacman::p_load("cols4all")      # color maps + evaluation 
+pacman::p_load("ggtext")        # more flexible text formatting in ggplot2
+pacman::p_load("directlabels")  # smart automatic direct labelling
+pacman::p_load("geomtextpath")  # label along lines
---
+++ src/custom.R
@@ -0,0 +1,10 @@
+knitr::opts_chunk$set(
+  class.output  = "bg-success",
+  class.message = "bg-info",
+  class.warning = "bg-warning",
+  class.error   = "bg-danger",
+  fig.retina = 4
+  )
+
+knitr::knit_hooks$set(crop = knitr::hook_pdfcrop)
+
